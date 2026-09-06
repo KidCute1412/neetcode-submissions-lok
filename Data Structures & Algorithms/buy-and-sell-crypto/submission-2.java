c4e1a624b6419a9781a7ce34652e381191ecffff
Lê Tuấn Lộc <161718796+KidCute1412@users.noreply.github.com>
2026-09-07 02:30:29 +0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int min = prices[0];
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (prices[i] < min)
+                min = prices[i];
+            ans = Integer.max(ans, prices[i] - min);
+        }
+        return ans;
+        
+    }
+}
