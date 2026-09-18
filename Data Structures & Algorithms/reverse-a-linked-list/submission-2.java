1e683499eee0eb87cbc4efa33612ea44fa2d72b9
Lê Tuấn Lộc <161718796+KidCute1412@users.noreply.github.com>
2026-09-18 11:51:48 +0700

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // Today is 18 / 9, still love DA
+        ListNode cur = head;
+        ListNode prev = null;
+        while (cur != null)
+        {
+            ListNode tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+    }
+}
